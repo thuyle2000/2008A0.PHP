b592a8d93675d4bc61dc000122c2036ed1353b9d
Le Mong Thuy <thuyle2000@gmail.com>
2021-05-14 17:16:13 +0700

    Upload May 14, 2021

--- d04_while_fibo.php
+++ d04_while_fibo.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,16 +11,16 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-<div class="container">
-        <h2>Fibonacci series : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 ... </h2>
+    <div class="container">
+        <h3>Fibonacci series : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 ... </h3>
         <hr>
 
         <form action="">
             Input N : <br>
-            <input type="number" name="n" id="n" value="5" required
-            min=0 max="20"> <br> <br>
-            
+            <input type="number" name="n" id="n" value="5" required min="1"> <br> <br>
+
             <input type="submit" value="Execute" name="btOK" class="btn btn-danger">
             <input type="reset" value="Reset" class="btn btn-info">
 
@@ -33,17 +34,30 @@
             echo "<hr>";
             echo "<h2> Fibonacci $n-numbers : </h2>";
 
-            $r = 1;
-            for($i=2 ; $i <= $n ; $i++){
-                $r *= $i;
-            }
-            
-            
+            echo " 0";
+            if ($n == 2) {
+                echo ", 1" ;
+            } else if ($n > 2) {
+                echo ", 1" ;
 
+                $t1 = 0;
+                $t2 = 1;
+
+                $count = 3;
+                while ($count <= $n) {
+
+                    $nextTerm = $t2 + $t1;
+                    echo ", $nextTerm";
+                    $t1 = $t2;
+                    $t2 = $nextTerm;
+                    $count++;
+                }
+            }
         }
 
         ?>
 
     </div>
 </body>
+
 </html>
