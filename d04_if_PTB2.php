20222de57177c4a3b212c108cead39304d001f08
Le Mong Thuy <thuyle2000@gmail.com>
2021-05-14 15:25:28 +0700

    upload May 14, 2021

---
+++ d04_if_PTB2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PTB2</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h2>Giai Phuong trinh bac 2 : ax<sup>2</sup> + bx + c = 0 </h2>
+
+        <form action="">
+            Nhap he so a : <br>
+            <input type="number" name="ha" id="ha" value="1" required> <br> <br>
+
+            Nhap he so b : <br>
+            <input type="number" name="hb" id="hb" value="1" required> <br> <br>
+
+            Nhap he so c : <br>
+            <input type="number" name="hc" id="hc" value="1" required> <br> <br>
+
+            <input type="submit" value="Calculate" name="btOK" class="btn btn-danger">
+            <input type="reset" value="Reset" class="btn btn-info">
+
+        </form>
+    </div>
+</body>
+
+</html>
