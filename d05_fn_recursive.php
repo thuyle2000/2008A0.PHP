c7d40dbfb80579217afcd07bb2586d4deb4574ed
Le Mong Thuy <thuyle2000@gmail.com>
2021-05-17 15:27:27 +0700

    Upload May 17, 2021

---
+++ d05_fn_recursive.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>function</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+    <h2>Function Recursive - PHP</h2>
+    <hr>
+
+    <form action="">
+    <input type="number" name="n" id="n" value="5" required> 
+    <input type="submit" value="submit" name="btOK" class="btn btn-danger">
+    </form>
+    
+    <?php 
+    if(isset($_GET["btOK"])){
+        $n = $_GET["n"];
+
+
+        echo "<hr>";
+        if($n>=0){
+            echo " $n! = ". fatorial($n) . "<br>";
+        }
+        else{
+            echo "5! = ". fatorial() . "<br>";
+        }
+    }
+    
+    //ham de qui tinh giai thua n
+    function fatorial($n = 5){
+        if($n==0){
+            return 1;
+        }
+
+        return $n * fatorial($n-1);
+    }
+    
+    ?>
+    
+    </div>
+</body>
+</html>
