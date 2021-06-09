aab57e0584147e49595147028fc0244c36a079b7
Le Mong Thuy <thuyle2000@gmail.com>
2021-06-09 15:57:50 +0700

    Upload - June 9, 2021

--- laravel/d02/routes/web.php
+++ laravel/d02/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\DB;
 
 /*
@@ -19,26 +20,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', [BookController::class,"get"]);
+
 Route::get('/login', [AccountController::class,"login"]);
 Route::post('/checklogin', [AccountController::class,"checkLogin"]);
+Route::get('/logout', [AccountController::class,"logout"]);
 
-Route::prefix('admin')->name('admin')->middleware('checkLogin')->group(function() {
+
+Route::prefix('admin')->name("admin")->middleware('checkLogin:admin')->group(function() {
     Route::get('users', [AccountController::class,"index"])->name('userlist');
     Route::get('create', [AccountController::class,"create"]);
     Route::post('post', [AccountController::class,"createPost"]);
     Route::get('resetpass/{id}',[AccountController::class,"resetPassword"]);
 });
-
 // Route::get('admin/users', [AccountController::class,"index"])->name("userlist");
 // Route::get("admin/create", [AccountController::class,"create"]);
 // Route::post("admin/post", [AccountController::class,"createPost"]);
 // Route::get('admin/resetpass/{id}', [AccountController::class,"resetPassword"]);
 
 
-
-Route::prefix('user')->name('user')->middleware('checkLogin')->group(function(){
+Route::prefix('user')->name('user')->middleware('checkLogin:admin,user')->group(function(){
     Route::get("profile/{id}", [AccountController::class,"details"]);
 });
-
 // Route::get("user/profile/{id}", [AccountController::class,"details"]);
 
